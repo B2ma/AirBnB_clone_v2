762948859d2aa6a57f3da3d19c5d4eccb838a640
B2ma <beatrice.terer@gmail.com>
2023-11-26 07:31:10 +0000

    updates 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,12 +2,15 @@
 """ This script starts a Flask web application """
 
 from flask import Flask, render_template
+from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
+    """ Outputs list of states """
     states = storage.all("State").values()
     states_sorted = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=states_sorted)
@@ -15,6 +18,7 @@ def states_list():
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
+    """ closes current session """
     storage.close()
 
 
