07e9abb1f8dd21e800e9c65eed9ebc395bcc121c
B2ma <beatrice.terer@gmail.com>
2023-11-26 09:44:39 +0000

    updates task 8,9,10

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,17 +9,26 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ Outputs list of states """
-    states = storage.all("State").values()
-    states_sorted = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states_sorted)
+    """
+    Display a HTML page with a list of states.
+
+    The list is sorted by state name.
+
+    Returns:
+        str: HTML content to be displayed.
+    """
+    states = storage.all(State)
+    states_list = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states_list)
 
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
-    """ closes current session """
+    """
+    Close the current SQLAlchemy Session after each request.
+    """
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000)
